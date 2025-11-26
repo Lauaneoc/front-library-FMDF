3a799c104c69090be1f29d40ab8e4a334c48b17f
Lauaneoc <oliveiralauane91@gmail.com>
2025-11-26 09:09:31 -0300

    feat: add ToasterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RoutesPage } from "./route"
+import { Toaster } from "./components/ui/toaster"
 
 function App() {
 
   return (
     <>
       <RoutesPage />
+      <Toaster />
     </>
   )
 }
