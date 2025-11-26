9dfa45d03a5c0b3159208881dff761c0464ac1c1
Lauaneoc <oliveiralauane91@gmail.com>
2025-11-26 09:10:05 -0300

    feat: add createOneBook function mutation in books contexts

--- src/@shared/contexts/livros/LivrosProvider.tsx
+++ src/@shared/contexts/livros/LivrosProvider.tsx
@@ -1,26 +1,52 @@
 import { createContext, ReactNode } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import type { LivroInterface } from "../../interfaces/livroInterface";
 import { livroService } from "../../services/livroService";
+import { toast } from "../../../hooks/use-toast";
 
 interface LivrosContextData {
   livros: LivroInterface[] | null;
   isLoading: boolean;
+  createOneBook: any;
 }
 
 export const LivrosContext = createContext<LivrosContextData | null>(null);
 
 export function LivrosProvider({ children }: { children: ReactNode }) {
+  const queryClient = useQueryClient();
+
   const { data: livros, isLoading } = useQuery({
     queryKey: ["livros"],
     queryFn: livroService.getAll,
   });
+
+  const createOneBook = useMutation({
+    mutationFn: (
+      value: LivroInterface
+    ) => livroService.create(value),
+    onSuccess: () => {
+      toast({
+        title: "Sucesso",
+        description: "Livro cadastrado com sucesso!",
+        variant: "default"
+      })
+      queryClient.invalidateQueries({ queryKey: ["livros"] })
+    },
+    onError: () => {
+      toast({
+          title: "Erro",
+          description: "Erro ao cadastrar livro!",
+          variant: "destructive"
+      })
+    },
+  })
 
   return (
     <LivrosContext.Provider
       value={{
         livros,
         isLoading,
+        createOneBook,
       }}
     >
       {children}
